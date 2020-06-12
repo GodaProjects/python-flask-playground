824bca26268d72d98af2cabf89285d20d9af4872
GodaProjects <goda.projects@gmail.com>
2020-06-12 21:26:40 +0530

    Templating using jinja 2

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
 
 # First step - init
-app =Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     # You dont have to mention the templates folder here as the name is predefined name.
     return render_template('index.html')
+
 
 # Why this condition is written without directly calling app.run is something i am not sure.
 if __name__ == "__main__":
