8cce21877d7fec7fd4c386bfa15536002b9d7324
GodaProjects <goda.projects@gmail.com>
2020-06-12 22:53:34 +0530

    Adding to database, retrieving from database and a json service

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import json
+
 
 # First step - init
 app = Flask(__name__)
@@ -12,20 +14,57 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///goda.db'
 db = SQLAlchemy(app)
 
 
-class Todo(db.Model):
+class Friends(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
-    name = db.Column(db.DateTime, default=datetime.utcnow)
+    date = db.Column(db.DateTime, default=datetime.utcnow)
 
     # Called whenever some data is inserted
     def __repr__(self):
         return '<Task %r>' % self.id
 
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    # You dont have to mention the templates folder here as the name is predefined name.
-    return render_template('index.html')
+    if request.method == 'POST':
+        # pass
+        task_content = request.form['name']
+        new_insert = Friends(name=task_content)
+        try:
+            db.session.add(new_insert)
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            print(str(e))
+            return "There was a database issue and the data could not be inserted"
+    if request.method == 'GET':
+        # You can also do a query.get_or_404(<id>) and then use the id to do a db.session.delete(id) which I am not going to do now in the interest of time
+        data = Friends.query.order_by(Friends.date).all()
+
+        # You dont have to mention the templates folder here as the name is predefined name.
+        return render_template('index.html', data=data)
+
+
+@app.route('/friend', methods=['GET'])
+def friend():
+    data = Friends.query.order_by(Friends.date).all()
+    data_lst = []
+    for item in data:
+        data_lst.append(
+            {
+                'name': item.name,
+                'date': item.date.date()
+            }
+        )
+    # It can be named anything not essentially response.
+    response = jsonify(data_lst)
+    response.status_code = 200
+    return response
+
+
+def toJSON(data):
+    return json.dumps(data, default=lambda o: o.__dict__,
+                      sort_keys=True, indent=4)
 
 
 # Why this condition is written without directly calling app.run is something i am not sure.
---
+++ static/js/scripts.js
@@ -0,0 +1,4 @@
+document.getElementById("add").addEventListener("click", (e) => {
+  console.log("In the event handler function");
+  document.getElementById("database").submit();
+});
