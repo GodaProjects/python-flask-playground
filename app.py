1affe8e19b7e01a78070cc573ca636bc2affa6b1
GodaProjects <goda.projects@gmail.com>
2020-06-12 21:12:53 +0530

    Create a basic html page with render_template

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # First step - init
 app =Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello, Goda!"
+    # You dont have to mention the templates folder here as the name is predefined name.
+    return render_template('index.html')
 
 # Why this condition is written without directly calling app.run is something i am not sure.
 if __name__ == "__main__":
